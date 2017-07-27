4fa5d7c70e4bf35c9401b11c57f85cd6158831ac
Nate Collings <ncollings@instructure.com>
2017-07-27 16:06:28 -0600

    Fix BridgeBlueprint typo
    Change-Id: I0fe4553ee211c3b98a3e6341204b47d9bc89147a
    Reviewed-on: https://gerrit.instructure.com/120781
    Reviewed-by: Jay Shaffer <jshaffer@instructure.com>
    Product-Review: Nate Collings <ncollings@instructure.com>
    QA-Review: Nate Collings <ncollings@instructure.com>
    Tested-by: Nate Collings <ncollings@instructure.com>

--- app/lib/bridge_cache/jobs/import_check.rb
+++ app/lib/bridge_cache/jobs/import_check.rb
@@ -9,7 +9,7 @@ module BridgeCache::Jobs
         BridgeCache::Jobs::ImportData.set(queue: self.queue_name).perform_later(bridge_base_url, bridge_api_key, bridge_api_secret, models, chain)
       elsif remote_data.status == BridgeBlueprint::Constants::STATUS_PENDING
         BridgeCache::Jobs::ImportCheck.set(queue: self.queue_name, wait: 30.seconds).perform_later(bridge_base_url, bridge_api_key, bridge_api_secret, models, chain)
-      elsif remote_data.status == BridgeBlueprint::Constants::STATUS_FAILED || remote_data.status == BridgeBluePrint::Constants::STATUS_NOT_FOUND
+      elsif remote_data.status == BridgeBlueprint::Constants::STATUS_FAILED || remote_data.status == BridgeBlueprint::Constants::STATUS_NOT_FOUND
         raise 'Bridge data dump download failed'
       end
     end
