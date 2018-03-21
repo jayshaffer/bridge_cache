42deddbd5562b0e2725c1e1926949e19a8b4b60c
Matt Maruri <mmaruri@instructure.com>
2018-03-21 16:12:15 -0600

    fix for moved external course id
    Change-Id: I4a7bb658156b75d6668075e28da60fe6aced78c0
    Reviewed-on: https://gerrit.instructure.com/144415
    Reviewed-by: Tyson Brown <tbrown@instructure.com>
    Product-Review: Matt Maruri <mmaruri@instructure.com>
    QA-Review: Matt Maruri <mmaruri@instructure.com>
    Tested-by: Matt Maruri <mmaruri@instructure.com>

--- app/models/bridge_cache/enrollment_profile.rb
+++ app/models/bridge_cache/enrollment_profile.rb
@@ -2,7 +2,7 @@ module BridgeCache
   class EnrollmentProfile < BridgeBaseModel
     extend BridgeCache::Data::BridgeModel
 
-    belongs_to :enrollable, primary_key: BridgeCache.primary_key, polymorphic: true
+    belongs_to :enrollable, primary_key: BridgeCache.primary_key, polymorphic: true, optional: true
     before_validation :namespace_enrollable_type
     before_save :parse_json
 
