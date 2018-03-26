5ba603a2b4323bef4f681f554ed845ea74b4078a
jshaffer <jshaffer@instructure.com>
2018-03-26 12:22:28 -0600

    Fixes missing check in migration file
    Change-Id: I9e4284e48de921bfeef742b114173bf7ef630698
    Reviewed-on: https://gerrit.instructure.com/144834
    Reviewed-by: Cody Tanner <ctanner@instructure.com>
    Product-Review: Jay Shaffer <jshaffer@instructure.com>
    QA-Review: Jay Shaffer <jshaffer@instructure.com>
    Tested-by: Jay Shaffer <jshaffer@instructure.com>

--- db/migrate/20180320190443_add_index_to_enrollment_profile.rb
+++ db/migrate/20180320190443_add_index_to_enrollment_profile.rb
@@ -1,5 +1,6 @@
 class AddIndexToEnrollmentProfile < ActiveRecord::Migration[5.1]
   def change
+    return unless BridgeCache.use_internal_database
     add_index :bridge_cache_enrollment_profiles, :config, using: :gin
   end
 end
